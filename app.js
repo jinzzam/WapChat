7c22e9cafc8f2aeff482c529b1841b17332c647a
jinzzam <firstmind_@naver.com>
2018-05-21 06:54:44 +0900

    add chat & socket.io module

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var chatRouter = require('./routes/chat');
 
 var app = express();
+var io = require('socket.io').listen(4000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,27 +17,39 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
+
+io.sockets.on('connection', function (socket) {
+    socket.on('join:room', function (data) {
+        console.log('room' + data.roomId);
+        socket.join('room' + data.roomId);
+    });
+    socket.on('chatReq', function (data) {
+        console.log(data);
+        io.sockets.in('room1').emit('chatRes', data.msg);
+    });
+});
