48d5e81154b43f632b49bd027902ca2dfa8dffdb
jinzzam <firstmind_@naver.com>
2018-05-21 07:01:44 +0900

    add login route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var chatRouter = require('./routes/chat');
+var loginRouter = require('./routes/login')
 
 var app = express();
 var io = require('socket.io').listen(4000);
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chat', chatRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
