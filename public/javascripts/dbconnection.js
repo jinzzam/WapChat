49d6bac48dea8d874019fc258329c62d78c660df
Hyeon <kunk62@naver.com>
2018-05-24 21:12:46 +0900

    각자 sql 서버 비밀번호 기입

--- public/javascripts/dbconnection.js
+++ public/javascripts/dbconnection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123',
+    password: '1234', /*서윤영 비밀번호:null, 박유진 비밀번호:123, 김예영, 김영현 비밀번호:1234*/
     port: '3306',
     database: 'mydb'
 });
