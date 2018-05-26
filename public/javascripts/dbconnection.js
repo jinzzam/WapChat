f3213d28be12049096a323103a5c99b89f541f9d
dokdokorea <7dpdud@naver.com>
2018-05-27 03:06:21 +0900

    Make an overall structure in 'localhost:3000/conference-chat'
    1. Deleted some lines that were automatically written after pulling process to my computer.(you may not need to check these lines)
    2. Overall, I mainly changed the 'conference.ejs' file.
    3. Added checkbox, conference-content, checkbox, and button.

--- public/javascripts/dbconnection.js
+++ public/javascripts/dbconnection.js
@@ -1,11 +1,11 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123', /*서윤영 비밀번호:null, 박유진 비밀번호:123, 김예영, 김영현 비밀번호:1234*/
-    port: '3306',
-    database: 'mydb'
-});
-connection.connect();
-
-module.exports = connection;
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '123', /*서윤영 비밀번호:null, 박유진 비밀번호:123, 김예영, 김영현 비밀번호:1234*/
+//     port: '3306',
+//     database: 'mydb'
+// });
+// connection.connect();
+//
+// module.exports = connection;
