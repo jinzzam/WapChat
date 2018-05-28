42eab547a5bc5691db504552b5f12767b726a047
jinzzam <firstmind_@naver.com>
2018-05-28 15:05:31 +0900

    add query logic in update info

--- routes/modifyInfo.js
+++ routes/modifyInfo.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var connection = require('../public/javascripts/dbconnection');
-
 var router = express.Router();
 
 /* GET home page. */
@@ -10,6 +9,17 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
 
+    var updatesql = "";
+
+    connection.query(updatesql, function (err, rows, fields) {
+        if (!err)
+            console.log('The solution is ', rows);
+        else
+            console.log('Error while performing Query.', err);
+
+    });
+    connection.end();
+    res.redirect("/");
 })
 
 module.exports = router;
