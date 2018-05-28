944caea876d69d5ad2c5f01a664db6d70170ccd2
jinzzam <firstmind_@naver.com>
2018-05-28 15:01:22 +0900

    edit some html tag attribute and add submit button

--- routes/modifyInfo.js
+++ routes/modifyInfo.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var connection = require('../public/javascripts/dbconnection');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('modifyInfo');
 });
+
+router.post('/', function (req, res, next) {
+
+})
 
 module.exports = router;
