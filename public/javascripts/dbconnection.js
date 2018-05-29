955a0d87f1076c8a5ff5c8399537ecc25204faba
Hyeon <kunk62@naver.com>
2018-05-29 20:36:59 +0900

    ignore failed

--- public/javascripts/dbconnection.js
+++ public/javascripts/dbconnection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: '0000',
     port: '3306',
     database: 'mydb'
 });
