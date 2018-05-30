675aaae2b064c2f0f5c37fb6fb139f4c7743e7f0
Hyeon <kunk62@naver.com>
2018-05-30 18:52:54 +0900

    conference page error fixed

--- routes/conference.js
+++ routes/conference.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var connection = require('../public/javascripts/dbconnection');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    if (req.session.user_id == undefined) {
-        res.render('login', {title: 'title'});
+    if (req.session.user_id != undefined) {
+        res.render('conference');
     } else {
-        res.redirect('/');
+        res.redirect('/login');
     }
 });
 
