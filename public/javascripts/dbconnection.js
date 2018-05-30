a1451a5ed42ecf45c6cbeb9f73ecb97aa78b1a30
yunyoung <freakfreak22@naver.com>
2018-05-30 20:04:46 +0900

    인덱스 페이지 다시 수정

--- public/javascripts/dbconnection.js
+++ public/javascripts/dbconnection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '0000',
+    password: '',
     port: '3306',
     database: 'mydb'
 });
