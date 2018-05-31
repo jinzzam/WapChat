ff48618be36489f7b01e00dd290a083932aec910
Hyeon <kunk62@naver.com>
2018-06-01 02:03:29 +0900

    memos in join&login.ejs

--- routes/join.js
+++ routes/join.js
@@ -19,17 +19,17 @@ router.post('/', function (req, res, next) {
     var verifypw = req.body.verify_pw;
     var nickname=req.body.user_nickname;
 
-    if (password == verifypw) {
+    if (password == verifypw) { //찾는 id의 db존재 여부에 따라 join.ejs의 중복체크 버튼에 사용가능여부 전달하고싶음.
         //회원가입 성공
         var insertsql = 'insert into user (name,birthday,number,id,password,nickname) values ("' + name + '","' + birthday + '","' + number + '","' + id + '","' + password + '","' + nickname + '")';
-        connection.query(insertsql, function (err, rows, fields) {
+        connection.query(insertsql, function (err, rows, field) {
             if (!err)
                 console.log('The solution is ', rows);
             else
                 console.log('Error while performing Query.', err);
         });
         connection.end();
-        res.redirect('/');
+        res.redirect('/login');
     } else {
         res.redirect('/join-fail');
     }
