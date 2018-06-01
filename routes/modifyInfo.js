ef7ac6293c052e00da2006560ca1d7413627509b
Hyeon <kunk62@naver.com>
2018-06-01 22:55:30 +0900

    merge temporarily fixed

--- routes/modifyInfo.js
+++ routes/modifyInfo.js
@@ -3,30 +3,6 @@ var connection = require('../public/javascripts/dbconnection');
 var router = express.Router();
 
 /* GET home page. */
-<<<<<<< HEAD
-=======
-
-router.post('/', function (req, res, next) {
-    var updatenamesql = 'select name from user where id=? and pw=?';
-    var updatebirthdaysql = 'select birthday date from user where id=? and pw=?';
-    var updatenumbsql = 'select  number from user where id=? and pw=?';
-    var updateidsql = 'select id from user where id=? and pw=?';
-    var updatepwsql = 'select password from user where id=? and pw=?';
-    var updatenicksql = 'select nickname from user where id=? and pw=?';
-    /* db 정보갱신 코드_변경 예정
-connection.query("UPDATE user SET name=?, birthday date=?, phonenumber=?,id=?,password=?,nickname=? where name=?, birthday date=?, phonenumber=?,id=?,password=?,nickname=?"
-    function (error, result, fields){
-            if(error){
-            res.send('err : '+error)
-            }
-            else {
-            console.log(name+','+birthday date+','+phonenumber+','+id+','+password+','+nickname)
-            res.send('success update user information: '+name+birthday date+phonenumber+id+password+nickname)
-            }
-    })
-connection.end();
-*/
-});
 
 router.get('/', function (req, res, next) {
     if (req.session.user_id != undefined) {
