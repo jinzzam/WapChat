21e50d8a1218cfdb1508200d643cd7cede755117
YeyoungK <clavisfloccu@naver.com>
2018-06-02 00:31:45 +0900

    add modify user information and modify err in chat.js (delete <h1>Wqp~~<h1>)

--- routes/modifyInfo.js
+++ routes/modifyInfo.js
@@ -3,40 +3,13 @@ var connection = require('../public/javascripts/dbconnection');
 var router = express.Router();
 
 /* GET home page. */
-<<<<<<< HEAD
-=======
-
-router.post('/', function (req, res, next) {
-    var updatenamesql = 'select name from user where id=? and pw=?';
-    var updatebirthdaysql = 'select birthday date from user where id=? and pw=?';
-    var updatenumbsql = 'select  number from user where id=? and pw=?';
-    var updateidsql = 'select id from user where id=? and pw=?';
-    var updatepwsql = 'select password from user where id=? and pw=?';
-    var updatenicksql = 'select nickname from user where id=? and pw=?';
-    /* db 정보갱신 코드_변경 예정
-connection.query("UPDATE user SET name=?, birthday date=?, phonenumber=?,id=?,password=?,nickname=? where name=?, birthday date=?, phonenumber=?,id=?,password=?,nickname=?"
-    function (error, result, fields){
-            if(error){
-            res.send('err : '+error)
-            }
-            else {
-            console.log(name+','+birthday date+','+phonenumber+','+id+','+password+','+nickname)
-            res.send('success update user information: '+name+birthday date+phonenumber+id+password+nickname)
-            }
-    })
-connection.end();
-*/
-});
-
 router.get('/', function (req, res, next) {
     if (req.session.user_id != undefined) {
-        res.render('modifyInfo',{title: 'Modify User Information'})
-        res.render('conference',{user_pw:req.session.user_pw});
-        res.render('conference',{verify_pw:req.session.verify_pw});
-        res.render('conference',{user_nickname:req.session.user_nickname});
-        res.render('conference',{user_name:req.session.user_name});
-        res.render('conference',{user_phonenumber:req.session.user_phonenumber});
-        res.render('conference',{user_birthday:req.session.user_birthday});
+        var select = 'select * from user where id=?';
+        connection.query(select, req.session.user_id, function (err, rows, fields) {
+            console.log(rows);
+            res.render('modifyInfo', {title: 'Modify User Information', udata: rows[0], session: req.session});
+        });
     } else {
         res.redirect('/login');
     }
@@ -44,68 +17,26 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res) {
     console.log(req.body);
-    var user_name=req.body.user_name;
-    var user_phone=req.body.number;
+    var user_name = req.body.username;
+    var user_phone = req.body.number;
     var user_id = req.body.user_id;
     var user_pw = req.body.user_pw;
-    var user_birthday=req.body.birthday;
-    var user_nickname=req.body.user_nickname;
+    var user_verify_pw = req.body.verify_pw;
+    var user_birthday = req.body.birthday;
+    var user_nickname = req.body.user_nickname;
 
-    if (password == verifypw) {
+    if (user_pw == user_verify_pw) {
         //정보수정
-        var updatename = "update user set values(name=?) where id=?";
-        var updatenumber ="update user set values(number=?) where id=?";
-        var updateid="update user set values(id=?) where id=?";
-        var updatepw="update user set values(password=?) where id=?";
-        var updatebirthday="update user set values(birthday=?) where id=?";
-        var updatenickname="update user set values(nickname=?) where id=?";
-        connection.query(updatename, function (err, rows, fields) {
+        var updatename = "update user set name=?,phonenumber=?,id=?,password=?,birthday=?,nickname=? where id=?";
+        connection.query(updatename, [user_name, user_phone, user_id, user_pw, user_birthday, user_nickname, user_id], function (err, rows, fields) {
             if (!err)
                 console.log('The solution is ', rows);
             else
                 console.log('Error while performing Query.', err);
-
+            res.redirect('/');
         });
-        connection.query(updatenumber, function (err, rows, fields) {
-            if (!err)
-                console.log('The solution is ', rows);
-            else
-                console.log('Error while performing Query.', err);
-
-        });
-        connection.query(updateid, function (err, rows, fields) {
-            if (!err)
-                console.log('The solution is ', rows);
-            else
-                console.log('Error while performing Query.', err);
-
-        });
-        connection.query(updatepw, function (err, rows, fields) {
-            if (!err)
-                console.log('The solution is ', rows);
-            else
-                console.log('Error while performing Query.', err);
-
-        });
-        connection.query(updatebirthday, function (err, rows, fields) {
-            if (!err)
-                console.log('The solution is ', rows);
-            else
-                console.log('Error while performing Query.', err);
-
-        });
-        connection.query(updatenickname, function (err, rows, fields) {
-            if (!err)
-                console.log('The solution is ', rows);
-            else
-                console.log('Error while performing Query.', err);
-
-        });
-        connection.end();
-        res.redirect('/');
     } else {
         res.redirect('/login');
     }
-    res.render('modifyInfo');
 });
 module.exports = router;
